c6490c38de3a57b39236ea9e27cf2e0a182ae0de
Indranil Bhattacharya <ibhattacharya1218@gmail.com>
2021-09-11 23:17:20 +0530

    New destructuring example added

--- destructuring.js
+++ destructuring.js
@@ -4,8 +4,19 @@ let a,b;
 console.log(a)
 console.log(b)
 
-//object destructuring
+//examples of object destructuring
+//1
 let name = '',age;
 ({name} = {name:'Max',age:28})
 console.log(name)
 console.log(age)
+
+//2
+const myObj = {
+    fname: 'Max',
+    age: 28
+}
+const {fname} = myObj;
+console.log(fname); // prints 'Max'
+console.log(age); // prints undefined
+console.log(myObj); // prints {name: 'Max', age: 28}
