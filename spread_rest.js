23afc8a903b7927e6def1b44b61851f7a2a70032
Indranil Bhattacharya <ibhattacharya1218@gmail.com>
2021-09-12 22:57:00 +0530

    Variable reference doesn't get copied

--- spread_rest.js
+++ spread_rest.js
@@ -1,22 +1,28 @@
 //examples of spread
 //1
-const numbers = [1, 2, 3, 4]
-const newNumbers = [...numbers, 5]
-console.log(newNumbers)
+const numbers = [1, 2, 3, 4];
+const newNumbers = [...numbers, 5];
+console.log(newNumbers);
 
 //2
 const person = {
-    name: 'Max'
-}
+  name: "Max",
+};
+
+//for variable reference doesn't get copied
+let a = 5;
+const b = a;
+a = 6;
+console.log(b);
 
 const newPerson = {
-    ...person,
-    age: 28
-}
-console.log(newPerson)
+  ...person,
+  age: 28,
+};
+console.log(newPerson);
 
 //examples of rest(arguement)
 const filter = (...args) => {
-    return args.filter(el => el === 1)
-}
-console.log(filter(1,2,3,4))
+  return args.filter((el) => el === 1);
+};
+console.log(filter(1, 2, 3, 4));
